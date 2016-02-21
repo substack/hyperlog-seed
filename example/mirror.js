94eee33a583b3e0f2cfe03227740902e1f76ff92
James Halliday <mail@substack.net>
2016-02-21 02:26:10 -0800

    working mirror code

--- example/mirror.js
+++ example/mirror.js
@@ -1,6 +1,9 @@
 var webtorrent = require('webtorrent')
 var parseTorrent = require('parse-torrent')
 var level = require('level')
+var fs = require('fs')
+var path = require('path')
+var mkdirp = require('mkdirp')
 var sub = require('subleveldown')
 var swarmlog = require('swarmlog')
 
@@ -22,17 +25,41 @@ var seeder = hseed({
     if (row.unlink) return { type: 'del', link: row.unlink }
   }
 })
+var dir = '/tmp/webtorrent'
 
 var client = webtorrent()
+
 seeder.on('seed', function (link) {
-  var t = parseTorrent(link)
-  client.add(link)
   console.log('SEED', link)
+  var t = parseTorrent(link)
+  var tdir = path.join(dir, t.infoHash)
+  fs.readdir(tdir, function (err, files) {
+    if (!files) client.add(link, onadd)
+    else {
+      var tfiles = files.map(function (f) { return path.join(tdir, f) })
+      client.seed(tfiles, onseed)
+    }
+  })
+  function onadd (s) {
+    var tdir = path.join(dir, t.infoHash)
+    mkdirp(tdir, function () {
+      t.files.forEach(function (file) {
+        file.createReadStream()
+          .pipe(fs.createWriteStream(path.join(tdir, t.infoHash)))
+      })
+    })
+  }
+  function onseed (s) {
+    if (s.infoHash !== t.infoHash) {
+      client.remove(s)
+      client.add(link, onadd)
+    }
+  }
 })
 seeder.on('unseed', function (link) {
+  console.log('UNSEED', link)
   var t = parseTorrent(link)
   for (var i = 0; i < client.torrents.length; i++) {
     if (client[i].infoHash === t.infoHash) client[i].destroy()
   }
-  console.log('UNSEED', link)
 })
